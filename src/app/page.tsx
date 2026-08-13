2ac42e5956051619e6f5550fed43840ce4599595
Richard Rivera <rojasrichard0603@outlook.com>
2026-08-13 12:50:24 -0400

    Añadir sección Hero con animación de escritura y traducciones; incluir imagen de perfil y estilos de cursor.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import Navbar from "./components/Navbar";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
       <Navbar />
 
       {/* Main Contaier */}
-      <main className="grow">
+      <main className="grow *:flex flex-col items-center justify-center w-full min-h-screen px-4 py-2 mx-auto max-w-7xl">
+        {/* Hero Section */}
+        <Hero />
         
       </main>
     </>
